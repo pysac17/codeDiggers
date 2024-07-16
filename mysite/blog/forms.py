34d8ca21428e17aea38452e9585dc57dccb2745a
Sachi017 <80409697+pysac17@users.noreply.github.com>
2024-07-17 05:18:58 +0530

    main
    main

---
+++ mysite/blog/forms.py
@@ -0,0 +1,18 @@
+from django import forms
+from .widgets import BootstrapDateTimePickerInput
+
+
+class DateForm(forms.Form):
+    date = forms.DateTimeField(
+        input_formats=['%d/%m/%Y %H:%M'],
+        widget=BootstrapDateTimePickerInput()
+    )
+
+# class DateForm(forms.Form):
+#     date = forms.DateTimeField(
+#         input_formats=['%d/%m/%Y %H:%M'],
+#         widget=forms.DateTimeInput(attrs={
+#             'class': 'form-control datetimepicker-input',
+#             'data-target': '#datetimepicker1'
+#         })
+#     )
